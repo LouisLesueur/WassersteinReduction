1a50ee54cfb7908ed3c75befa63c0ef78564fb6b
Louis Lesueur <louis.lesueur@gmx.fr>
2022-01-05 16:40:11 +0100

    multiple trials

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
     a = torch.zeros((k,T)) # weights
     for s in range(k):
-        a[s] = Gaussian(s-4, 1)
+        a[s] = Gaussian(s, 1)
 
     lambdas = torch.ones(k)/k # lambdas
 
@@ -46,27 +46,45 @@ if __name__ == "__main__":
     cost1 = cost(K, couplings.to(device))
     print(f"bary 1: computed in {t1-t0}s, cost={cost1}")
 
-    M = torch.arange(1, 30, 1)
-    costs = torch.zeros(M.shape[0])
+    ref_time = t1-t0
 
-    bary_dif = []
+    max_m = 20
+    n_trials = 5
+    
+    M = torch.arange(max_m)
+    
+    costs = torch.zeros((n_trials, max_m))
+    bary_dif = torch.zeros((n_trials, max_m))
+    speedup = torch.zeros((n_trials, max_m))
+
     loss = torch.nn.MSELoss()
 
-    for i,m in enumerate(M):
+    for trial in range(n_trials):
 
-        dimsolver = JLProjWasserstein(solver, m, device=device)
-        t2 = time.time()
-        bary2, couplings2, K2 = dimsolver.solve(lambdas, X, a, support )
-        t3 = time.time()
-        cost2 = cost(K2, couplings.to(device))
-        print(f"bary 2: computed in {t3-t2}s, cost={cost2}")
+        for i,m in enumerate(M):
 
-        print("cost ratio: ", cost2/cost1)
-        costs[i] = cost2/cost1
+            dimsolver = JLProjWasserstein(solver, m, device=device)
+            t2 = time.time()
+            bary2, couplings2, K2 = dimsolver.solve(lambdas, X, a, support )
+            t3 = time.time()
+            cost2 = cost(K2, couplings.to(device))
+            print(f"bary 2: computed in {t3-t2}s, cost={cost2}")
 
-        bary_dif.append(loss(bary, bary2))
-        
-    plt.plot(M.cpu(), bary_dif)
+            red_time = t3 - t2
+
+            print("cost ratio: ", cost2/cost1)
+            
+            costs[trial, i] = cost2/cost1
+            bary_dif[trial, i] = loss(bary, bary2)
+            speedup[trial, i] = red_time/ref_time
+
+
+    plt.errorbar(M.cpu(), torch.mean(bary_dif, axis=0), torch.std(bary_dif, axis=0), ecolor='lightgray')
+    plt.title("L2 difference between the barycenter weights")
     plt.show()
-    plt.plot(M.cpu(), costs.cpu())
+    plt.errorbar(M.cpu(), torch.mean(costs, axis=0), torch.std(costs, axis=0), ecolor='lightgray')
+    plt.title("Cost ratio")
+    plt.show()
+    plt.errorbar(M.cpu(), torch.mean(speedup, axis=0), torch.std(speedup, axis=0), ecolor='lightgray')
+    plt.title("Speedup")
     plt.show()
